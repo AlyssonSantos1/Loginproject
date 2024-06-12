f986e3b9da899cb8a3b37d8bd81f27201afc6644
AlyssonSantos1 <salysson280@gmail.com>
2024-06-12 17:15:13 -0300

    update

--- system.php
+++ system.php
@@ -16,7 +16,7 @@
 
     $result = $conexao->query($sql);
 
-    print_r($result);
+    // print_r($result);
 
 ?>
 
@@ -42,8 +42,8 @@
     <?php
         echo "<h1> Welcome <u>$logado</u> </h1>";
     ?>
-    <div>
-    <table class="table">
+    <div class="m-5">
+    <table class="table text-red">
     <thead>
         <tr>
             <th scope="col">Nome | Name</th>
@@ -57,8 +57,9 @@
     </thead>
     <tbody>
         <?php
-        while($user_data = mysqli_fetch_assoc($result));
-        
+
+        while($user_data = mysqli_fetch_assoc($result))
+        {      
 
             echo "<tr>";
             echo "<td>". $user_data['nome']."</td>";
@@ -68,8 +69,16 @@
             echo "<td>". $user_data['idade']."</td>";
             echo "<td>". $user_data['senha']."</td>";
             
-            echo "<td>"
+            echo "<td>;
+                <a class= 'btn btn-primary' href='edit.php?id=$user_data[id]'>
 
+            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325'/>
+                </svg>
+                </a>
+                </td>";
+        
+        }
         ?>
         
     </tbody>
